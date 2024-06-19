b11f7bfd5f85c1a10e4bd70b55670ba0144b3df1
Bevsworld <collab.bevsworld@gmail.com>
2024-06-19 23:42:19 +0200

    yes

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import Home from './app.js';
+
+
+const routes = [
+    {
+        path: '/',
+        exact: true,
+        component: Home,
+    },
+
+    // Add all your routes here
+];
+
+export default routes;
